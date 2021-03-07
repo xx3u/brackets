fbe279be76e0df8977b78ab563d397a55f94cd9d
YK <yuliya.khv@gmail.com>
2021-03-07 09:24:22 +0600

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newAr = [];
+  let brackets = bracketsConfig.flat();
+  let duplicates = brackets.filter((item, index) => brackets.indexOf(item) !== index).flat();
+  // let count = {};
+  // console.log(duplicates)
+
+  for (let i = 0; i < str.length; i++) {
+    let index = brackets.indexOf(str[i]);
+    // console.log('i', i, 'index', index)
+    if (index % 2 === 0) {
+      let dupl = newAr.indexOf(duplicates.find(el => el === brackets[index + 1]));
+      // console.log('dupl', dupl)
+      if (dupl !== -1 && i > index) {
+        newAr.splice(dupl, 1);
+        // console.log('newAr.splice', newAr)
+      } else {
+        newAr.push((brackets[index + 1]));
+        // console.log('newAr.push', newAr)
+      }
+    } else {
+      // console.log('newAr.pop', newAr.pop(), 'index', index);
+      if(newAr.pop() !== brackets[index]) {
+        // console.log('2) newAr.pop', newAr);
+        return false;
+      };
+    } 
+  };
+  // console.log('newAr', newAr);
+  // if (duplicates.length !== 0) {
+  //   newAr.forEach(el => {count[el] = (count[i] || 0) + 1});
+  //   console.log('count', count)
+  // } else {
+    return newAr.length === 0
+  // }
 }
